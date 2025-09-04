3faddde8e0949b24579d2d9744e8ecb5f75b62d9
EELMMAD <elmiramadadi@gmail.com>
2025-09-04 11:09:20 +0200

    add dependency and constructor injection

---
+++ src/main/java/com/codewithelmira/store/PaymentService.java
@@ -0,0 +1,5 @@
+package com.codewithelmira.store;
+
+public interface PaymentService {
+    void processPayment(double amount);
+}
---
+++ src/main/java/com/codewithelmira/store/PaypalPaymentService.java
@@ -0,0 +1,9 @@
+package com.codewithelmira.store;
+
+public class PaypalPaymentService implements PaymentService{
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("PAYPAL");
+        System.out.println("Amount " + amount + " paid.");
+    }
+}
