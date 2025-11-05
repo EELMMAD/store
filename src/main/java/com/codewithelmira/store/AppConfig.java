19e064c17c25f49f50363fb9a346e6865908f8d2
EELMMAD <elmiramadadi@gmail.com>
2025-11-05 18:22:49 +0100

    configuring beans using code

---
+++ src/main/java/com/codewithelmira/store/AppConfig.java
@@ -0,0 +1,28 @@
+package com.codewithelmira.store;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Value("${stripe.payment-gateway: stripe}")
+    private String paymentGateway;
+
+    public PaymentService stripe(){
+        return new StripePaymentService();
+    }
+
+    @Bean
+   public PaymentService paypal(){
+        return new PaypalPaymentService();
+    }
+    @Bean
+    public OrderService orderService(){
+        if(paymentGateway.equals("stripe")){
+            return new OrderService(stripe());
+        }
+        return new OrderService(paypal());
+    }
+}
